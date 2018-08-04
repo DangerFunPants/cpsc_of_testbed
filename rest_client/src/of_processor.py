d5c8b819340766a896230b4b5824222e9ea80aa4
Alexander James <alexander.james@ucalgary.ca>
2018-08-04 16:17:49 -0600

    Type annotations.

--- rest_client/src/of_processor.py
+++ rest_client/src/of_processor.py
@@ -1,61 +1,72 @@
 import of_rest_client as of
 import flowmod as fm
 
+from typing import Any
 
 class OFProcessor:
 
-    def __init__(self, of_controller_ip, of_controller_port):
+    def __init__( self              : OFProcessor
+                , of_controller_ip  : str
+                , of_controller_port: str ) -> None:
         self._of_controller_ip = of_controller_ip
         self._of_controller_port = of_controller_port
 
-    def _curry_of_msg_cons(self, cons):
+    def _curry_of_msg_cons(self: OFProcessor, cons: Callable[Any, Any]) -> Any:
         return (lambda args : cons(*args, self._of_controller_ip, self._of_controller_port))
 
-    def get_switch_list(self):
+    def get_switch_list(self: OFProcessor) -> List[int]:
         req = self._curry_of_msg_cons(of.SwitchList)
         resp = req([]).get_response()
         return resp.get_sw_list()
 
-    def get_switch_flows(self, dpid):
+    def get_switch_flows(self: OFProcessor, dpid: str) -> of.OFResposeSwitchFlows:
         req = self._curry_of_msg_cons(of.SwitchFlows)
         resp = req([dpid]).get_response()
         return resp
 
-    def push_flow_mod(self, dpid, flow_mod):
+    def push_flow_mod( self     : OFProcessor
+                     , dpid     : str
+                     , flow_mod : fm.FlowMod ) -> of.OFStatusResponse:
         req = self._curry_of_msg_cons(of.PushFlowmod)
         resp = req([flow_mod]).get_response()
         return resp
     
-    def get_topo_links(self):
+    def get_topo_links(self: OFProcessor) -> of.OFResponseTopologyLinks:
         req = self._curry_of_msg_cons(of.TopologyLinks)
         resp = req([]).get_response()
         return resp
     
-    def get_switch_desc(self, dpid):
+    def get_switch_desc(self: OFProcessor, dpid: str) -> of.OFResponseSwitchDesc:
         req = self._curry_of_msg_cons(of.SwitchDesc)
         resp = req([dpid]).get_response()
         return resp
 
-    def remove_all_flows(self, dpid):
+    def remove_all_flows(self: OFProcessor, dpid: str) -> of.OFStatusResponse:
         req = self._curry_of_msg_cons(of.RemoveAllFlows)
         resp = req([dpid]).get_response()
         return resp
 
-    def remove_flow(self, dpid, flow_mod):
+    def remove_flow( self       : OFProcessor
+                   , dpid       : str
+                   , flow_mod   : fm.FlowMod ) -> of.OFStatusResponse:
         req = self._curry_of_msg_cons(of.RemoveFlow)
         resp = req([dpid, flow_mod]).get_response()
         return resp
 
-    def get_port_stats(self, dpid):
+    def get_port_stats(self: OFProcessor, dpid: str) -> of.OFPortStatsResponse:
         req = self._curry_of_msg_cons(of.GetPortStats)
         resp = req([dpid]).get_response()
         return resp
 
-    def remove_table_flows(self, dpid, table_id):
+    def remove_table_flows( self        : OFProcessor
+                          , dpid        : str
+                          , table_id    : int ) -> of.OFStatusResponse:
         flow_mod = fm.Flowmod(dpid, table_id=table_id)
         return self.remove_flow(dpid, flow_mod)
 
-    def add_default_route(self, dpid, table_id):
+    def add_default_route( self     : OFProcessor
+                         , dpid     : str
+                         , table_id : int) -> of.OFStatusResponse:
         flow_mod = fm.Flowmod(dpid, priority=1, table_id=100)
         flow_mod.add_action(fm.Action(fm.ActionTypes.Output, {'port':4294967293}))
         self.push_flow_mod(dpid, flow_mod)

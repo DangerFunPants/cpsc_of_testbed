4ce0103e95b0bc3f3aa59b64d84db73efbd2c82b
Alexander James <alexander.james@ucalgary.ca>
2019-07-16 21:16:49 -0600

    Remove random semicolons.

--- util_scripts/exec_on_all.sh
+++ util_scripts/exec_on_all.sh
@@ -1,5 +1,7 @@
-for ip in $(dig @192.168.0.2 management.cpsc. axfr | egrep "host[0-9]{1,2}.*A" | awk '{print $5}'); 
+ip_list=$(dig @10.0.0.1 -t axfr hosts.sdn. | egrep "host[0-9]{1,2}.*A" | awk '{print $5}')
+echo $ip_list
+for ip in $ip_list
 do 
     echo "Host $ip with command $1";
-    sshpass -pcpsc ssh alexj@$ip "$1"; 
+    sshpass -pcpsc ssh alexj@$ip "$1";
 done

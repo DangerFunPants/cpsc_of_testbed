c3d036742c137c8eafbb0313bd69ae235d36d82d
Alexander James <alexander.james@ucalgary.ca>
2020-06-20 15:45:22 -0600

    Tuiti configuration parameters.

---
+++ rest_client/tuiti/config.py
@@ -0,0 +1,6 @@
+import pathlib  as path
+
+RESULTS_REPOSITORY_PATH = path.Path("/home/alexj/results-repositories/")
+base_repository_path = RESULTS_REPOSITORY_PATH  / "testing"
+repository_schema = "/provider-name"
+repository_name = "tuiti-testing"
